2722d46e4bdb33d3be8c199ed06f4fccf9374ea7
Bruno Cardoso Cantisano <bruno.cantisano@gmail.com>
2018-03-20 17:31:37 -0500

    fixing to use in linux

--- src/test/java/org/jenkinsci/plugins/ftpRename/FtpRenameTest.java
+++ src/test/java/org/jenkinsci/plugins/ftpRename/FtpRenameTest.java
@@ -8,13 +8,13 @@ import hudson.EnvVars;
 import hudson.slaves.*;
 
 public class FtpRenameTest {
-	FtpHandler ftpHandler = new FtpHandler();	
+	FtpHandler ftpHandler = new FtpHandler();
 	GlobalEnvironment gblEnv = new GlobalEnvironment();
 	EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
-	
-	private void addVariable(String key, String value) throws IOException {        		
+
+	private void addVariable(String key, String value) throws IOException {
 		EnvVars envVars = prop.getEnvVars();
-		envVars.put(key, value);        
+		envVars.put(key, value);
 	}
 
 	@Test
@@ -23,7 +23,7 @@ public class FtpRenameTest {
 		String value = "$globalKey";
 		assertNotEquals(value, gblEnv.replaceGlobalVars(value, prop.getEnvVars()));
 	}
-	
+
 	@Test
 	public void testSimpleBracesReplacement() throws IOException {
 		addVariable("globalKey", "globalValue");
@@ -51,40 +51,38 @@ public class FtpRenameTest {
 		String value = "${}";
 		assertEquals(value, gblEnv.replaceGlobalVars(value, prop.getEnvVars()));
 	}
-	
+
 	@Test
 	public void testChangeRemoteDirectory() {
 		assertTrue(ftpHandler.openConnection("ftp.dlptest.com", 21, "dlpuser@dlptest.com", "eiTqR7EMZD5zy7M", true));
 		assertTrue(ftpHandler.changeDirectory("/"));
-        assertTrue(ftpHandler.closeConnection());		    
+		assertTrue(ftpHandler.closeConnection());
 	}
 	@Test
 	/**
-	 * Public FTP test site info is below and can be used to upload test DLP files. 
+	 * Public FTP test site info is below and can be used to upload test DLP files.
      * The files will only be stored for 5 minutes before being deleted.
      * https://dlptest.com/ftp-test/
 	 */
 	public void testRenameFile() {
-		assertTrue(ftpHandler.openConnection("ftp.dlptest.com", 21, "dlpuser@dlptest.com", "eiTqR7EMZD5zy7M", true));		    	
+		assertTrue(ftpHandler.openConnection("ftp.dlptest.com", 21, "dlpuser@dlptest.com", "eiTqR7EMZD5zy7M", true));
 		assertTrue(ftpHandler.changeDirectory("/"));
-		
 		FileHandler tempFile = new FileHandler();
 		File file1 = tempFile.createFile("./teste1.txt", "testeteste");
 		File file2 = tempFile.createFile("./teste2.txt", "opa");
-		
 		assertTrue(ftpHandler.uploadFile(file1, "teste1.txt"));
 		assertTrue(ftpHandler.uploadFile(file2, "teste2.txt"));
 		//renaming...
         assertTrue(ftpHandler.renameFtpFile("teste1.txt", "teste2.txt", "/"));
-        
+
         //check sizes
-        assertEquals(5, ftpHandler.getFileSize("teste1.txt"));
-        assertEquals(12, ftpHandler.getFileSize("teste2.txt"));
-        
+        assertEquals(4, ftpHandler.getFileSize("teste1.txt"));
+        assertEquals(11, ftpHandler.getFileSize("teste2.txt"));
+
         //removing local files
         assertTrue(file1.delete());
         assertTrue(file2.delete());
-                
+
         assertTrue(ftpHandler.closeConnection());
-	}
+    }
 }
